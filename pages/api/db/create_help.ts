24ca19ac1a3e19f21a896efa5f294c137c103f32
IkkoKojima <panteranerow@gmail.com>
2020-08-10 01:05:28 +0900

    wip

--- pages/api/db/create_help.ts
+++ pages/api/db/create_help.ts
@@ -20,7 +20,7 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
         body: body,
         timebox: Number(timebox),
         fee: Number(fee)
-    }).then(result => res.status(200).json({ help_id: help_id })).catch(err => res.status(500).json({ error: err.message }))
+    }).then(_result => res.status(200).json({ help_id: help_id })).catch(err => res.status(500).json({ error: err.message }))
 }
 
 export default handler
